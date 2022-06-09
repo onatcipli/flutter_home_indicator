4ac0b1d8bb0226c944722bef12fe1ed86d7fd8ad
Onat Çipli <onatcipli@outlook.com>
2022-06-09 10:22:28 +0300

    fix import

--- ios/Classes/HomeIndicatorPlugin.m
+++ ios/Classes/HomeIndicatorPlugin.m
@@ -1,5 +1,10 @@
 #import "HomeIndicatorPlugin.h"
-#import <home_indicator-Swift.h>
+//#import <home_indicator/home_indicator-Swift.h>
+#if __has_include(<home_indicator/home_indicator-Swift.h>)
+#import <home_indicator/home_indicator-Swift.h>
+#else
+#import "home_indicator-Swift.h"
+#endif
 
 @implementation HomeIndicatorPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
